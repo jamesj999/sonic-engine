c501dbaa69eb6e281dbdb8ee97cc7ee1b0ea2356
james <jamesj999@gmail.com>
2017-09-11 17:59:03 +0100

    Reformatted all code to use spaces instead of tabs (yuck). Started work on adding SpriteRunningMode functionality to Tile.

--- src/main/java/uk/co/jamesj999/sonic/level/Tile.java
+++ src/main/java/uk/co/jamesj999/sonic/level/Tile.java
@@ -1,79 +1,106 @@
 package uk.co.jamesj999.sonic.level;
 
-import org.apache.commons.lang3.ArrayUtils;
+import uk.co.jamesj999.sonic.sprites.playable.SpriteRunningMode;
 
 public class Tile {
-	public byte[] heights;
-	private byte angle;
-	private boolean jumpThrough;
+   public byte[] groundHeights;
+   public byte[] rightWallHeights;
+   private byte angle;
+   private boolean jumpThrough;
 
-	public Tile(byte[] heights, byte angle) {
-		this(heights, angle, false, false);
-	}
+   public Tile(byte[] groundHeights, byte[] rightWallHeights, byte angle) {
+      this(groundHeights, rightWallHeights, angle, false, false);
+   }
 
-	private Tile(byte[] heights, byte angle, boolean flipX, boolean flipY) {
-		if (heights.length == 16) {
-			this.heights = heights;
-			if (flipX) {
-				ArrayUtils.reverse(heights);
-			}
-			if(flipY) {
-				reverseY();
-			}
-			// TODO add angle recalculations
-		} else {
-			System.out.println("Error flipping tile...");
-		}
-		this.angle = angle;
-	}
+   private Tile(byte[] groundHeights, byte[] rightWallHeights, byte angle, boolean flipX, boolean flipY) {
+      if (groundHeights.length == 16) {
+         this.groundHeights = groundHeights;
 
-	/**
-	 * Shorthand to create a tile without having to create the height map
-	 * separately.
-	 */
-	public Tile(int a, int b, int c, int d, int e, int f, int g, int h, int i,
-			int j, int k, int l, int m, int n, int o, int p, byte angle, boolean jumpThrough) {
-		heights = new byte[16];
-		heights[0] = (byte) a;
-		heights[1] = (byte) b;
-		heights[2] = (byte) c;
-		heights[3] = (byte) d;
-		heights[4] = (byte) e;
-		heights[5] = (byte) f;
-		heights[6] = (byte) g;
-		heights[7] = (byte) h;
-		heights[8] = (byte) i;
-		heights[9] = (byte) j;
-		heights[10] = (byte) k;
-		heights[11] = (byte) l;
-		heights[12] = (byte) m;
-		heights[13] = (byte) n;
-		heights[14] = (byte) o;
-		heights[15] = (byte) p;
-		this.angle = angle;
-		this.jumpThrough = jumpThrough;
-	}
+      } else {
+         //TODO: Proper exception handling
+         System.out.println("A tile doesn't have the correct length rightWallHeights array...");
+      }
+      if (rightWallHeights.length == 16) {
+         this.rightWallHeights = rightWallHeights;
+      } else {
+         //TODO: Proper exception handling
+         System.out.println("A tile doesn't have the correct length rightWallHeights array...");
+      }
 
-	public byte getHeightAt(byte y) {
-		return heights[y];
-	}
+      // TODO add angle recalculations - if the tile is flipped then the angle needs to be recalculated.
+      this.angle = angle;
+   }
 
-	public byte getAngle() {
-		return angle;
-	}
+   /**
+    * Shorthand to create a tile without having to create the height map
+    * separately. a-p are ground heights and a1-p1 are right wall heights.
+    */
+   public Tile(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k, int l, int m, int n, int o,
+               int p, int a1, int b1, int c1, int d1, int e1, int f1, int g1,int h1, int i1, int j1, int k1, int l1,
+               int m1, int n1, int o1, int p1, byte angle, boolean jumpThrough) {
+      groundHeights = new byte[16];
+      groundHeights[0] = (byte) a;
+      groundHeights[1] = (byte) b;
+      groundHeights[2] = (byte) c;
+      groundHeights[3] = (byte) d;
+      groundHeights[4] = (byte) e;
+      groundHeights[5] = (byte) f;
+      groundHeights[6] = (byte) g;
+      groundHeights[7] = (byte) h;
+      groundHeights[8] = (byte) i;
+      groundHeights[9] = (byte) j;
+      groundHeights[10] = (byte) k;
+      groundHeights[11] = (byte) l;
+      groundHeights[12] = (byte) m;
+      groundHeights[13] = (byte) n;
+      groundHeights[14] = (byte) o;
+      groundHeights[15] = (byte) p;
 
-	public Tile copy(boolean flipX, boolean flipY) {
-		return new Tile(heights, angle, flipX, flipY);
-	}
-	
-	private void reverseY() {
-		for(int i = 0; i < heights.length; i++) {
-			heights[i] = (byte) (16 - heights[i]);
-		}
-	}
-	
-	public boolean getJumpThrough() {
-		return jumpThrough;
-	}
+      rightWallHeights = new byte[16];
+      rightWallHeights[0] = (byte) a1;
+      rightWallHeights[1] = (byte) b1;
+      rightWallHeights[2] = (byte) c1;
+      rightWallHeights[3] = (byte) d1;
+      rightWallHeights[4] = (byte) e1;
+      rightWallHeights[5] = (byte) f1;
+      rightWallHeights[6] = (byte) g1;
+      rightWallHeights[7] = (byte) h1;
+      rightWallHeights[8] = (byte) i1;
+      rightWallHeights[9] = (byte) j1;
+      rightWallHeights[10] = (byte) k1;
+      rightWallHeights[11] = (byte) l1;
+      rightWallHeights[12] = (byte) m1;
+      rightWallHeights[13] = (byte) n1;
+      rightWallHeights[14] = (byte) o1;
+      rightWallHeights[15] = (byte) p1;
+
+      this.angle = angle;
+      this.jumpThrough = jumpThrough;
+   }
+
+   public byte getHeightAt(byte y, SpriteRunningMode spriteRunningMode) {
+      switch(spriteRunningMode) {
+         case GROUND:
+            return groundHeights[y];
+         case RIGHTWALL:
+            return rightWallHeights[y];
+         case CEILING:
+            return (byte) (16 - groundHeights[y]);
+         case LEFTWALL:
+            return (byte) (16 - rightWallHeights[y]);
+      }
+   }
+
+   public byte getAngle() {
+      return angle;
+   }
+
+   public Tile copy(boolean flipX, boolean flipY) {
+      return new Tile(groundHeights, rightWallHeights angle, flipX, flipY);
+   }
+
+   public boolean getJumpThrough() {
+      return jumpThrough;
+   }
 
 }
