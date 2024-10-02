8e32620db0279ec96c9bf4071781f1fe78f5f139
James <james.johnstone@bravebison.com>
2024-10-02 16:03:21 +0100

    Refactoring LevelManager to add correct logic (sorry for formatting change)

--- src/main/java/uk/co/jamesj999/sonic/level/LevelManager.java
+++ src/main/java/uk/co/jamesj999/sonic/level/LevelManager.java
@@ -1,36 +1,90 @@
 package uk.co.jamesj999.sonic.level;
 
+import com.jogamp.opengl.GL2;
+import uk.co.jamesj999.sonic.camera.Camera;
 import uk.co.jamesj999.sonic.configuration.SonicConfiguration;
 import uk.co.jamesj999.sonic.configuration.SonicConfigurationService;
 import uk.co.jamesj999.sonic.data.Game;
 import uk.co.jamesj999.sonic.data.Rom;
 import uk.co.jamesj999.sonic.data.games.Sonic2;
+import uk.co.jamesj999.sonic.graphics.GLCommand;
+import uk.co.jamesj999.sonic.graphics.GLCommandGroup;
+import uk.co.jamesj999.sonic.graphics.GraphicsManager;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LevelManager {
-	private static LevelManager levelManager;
-	private Level level;
-	private Rom rom;
+    private static LevelManager levelManager;
+    private Level level;
+    private Rom rom;
+    private GraphicsManager graphicsManager;
 
-	public void loadLevel(int levelIndex) throws IOException {
-		rom.open(SonicConfigurationService.getInstance().getString(SonicConfiguration.ROM_FILENAME));
-		Game game = new Sonic2(rom);
-		level = game.loadLevel(levelIndex);
-	}
+    public void loadLevel(int levelIndex) throws IOException {
+        rom.open(SonicConfigurationService.getInstance().getString(SonicConfiguration.ROM_FILENAME));
+        Game game = new Sonic2(rom);
+        level = game.loadLevel(levelIndex);
+    }
 
-	public void draw() {
-		// TODO
-	}
+    public void draw() {
+        // Old method. Need to update method of getting current tile for x/y coords and refactor accordingly.
 
-	public Level getCurrentLevel() {
-		return level;
-	}
+        // Work out our bounds. We don't want to be rendering or iterating tiles
+        // which are off screen.
+        Camera camera = Camera.getInstance();
+        int cameraX = camera.getX();
+        int cameraY = camera.getY();
+        int cameraWidth = camera.getWidth();
+        int cameraHeight = camera.getHeight();
+        int xLeftBound = cameraX / 16;
+        int xRightBound = (cameraX + cameraWidth) / 16;
+        int yBottomBound = cameraY / 16;
+        int yTopBound = (cameraY + cameraHeight) / 16;
+        List<GLCommand> commands = new ArrayList<GLCommand>();
+        for (int x = xLeftBound; x <= xRightBound; x++) {
+            int realX = x * 16;
+            for (int y = yBottomBound; y <= yTopBound; y++) {
+                int realY = y * 16;
+                SolidTile solidTile = getSolidTileAt(realX, realY);
+                if (solidTile != null) {
+                    for (int heightX = 0; heightX < SolidTile.TILE_SIZE_IN_ROM; heightX++) {
+                        int height = solidTile.getHeightAt((byte) heightX);
+                        if (height > 0) {
+                            for (int i = height + realY; i >= realY; i--) {
+                                commands.add(new GLCommand(
+                                        GLCommand.Type.VERTEX2I, -1, 1, 1,
+                                        1, realX + heightX, i, -1, -1));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        getGraphicsManager().registerCommand(new GLCommandGroup(GL2.GL_POINTS,
+                commands));
+    }
 
-	public synchronized static LevelManager getInstance() {
-		if (levelManager == null) {
-			levelManager = new LevelManager();
-		}
-		return levelManager;
-	}
+    public SolidTile getSolidTileAt(int x, int y) {
+        // TODO
+        return null;
+    }
+
+    private GraphicsManager getGraphicsManager() {
+        if (graphicsManager == null) {
+            graphicsManager = GraphicsManager.getInstance();
+        }
+        return graphicsManager;
+    }
+
+    public Level getCurrentLevel() {
+        return level;
+    }
+
+    public synchronized static LevelManager getInstance() {
+        if (levelManager == null) {
+            levelManager = new LevelManager();
+        }
+        return levelManager;
+    }
 }
