cac512322a888e7233f7b684ddb2b3a357985002
Farrell <farrell@bluetoaster.net>
2025-12-08 20:08:53 +0000

    Fix pitch of base note

--- src/main/java/uk/co/jamesj999/sonic/audio/smps/Sonic2SmpsData.java
+++ src/main/java/uk/co/jamesj999/sonic/audio/smps/Sonic2SmpsData.java
@@ -131,6 +131,6 @@ public class Sonic2SmpsData extends AbstractSmpsData {
 
     @Override
     public int getBaseNoteOffset() {
-        return 11; // Sonic 2 uses Base Note B (+1 offset for 0x81)
+        return 12; // Sonic 2 uses Base Note B (+12 offset for 0x81)
     }
 }
