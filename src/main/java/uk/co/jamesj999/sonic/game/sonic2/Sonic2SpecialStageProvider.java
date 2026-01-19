1985292cb515a35189b2e5bb8160781718673a83
Farrell <farrell@bluetoaster.net>
2026-01-19 17:37:13 +0000

    NoOp instances when things either don't exist, or aren't implemented yet. Replaces the need to null check

--- src/main/java/uk/co/jamesj999/sonic/game/sonic2/Sonic2SpecialStageProvider.java
+++ src/main/java/uk/co/jamesj999/sonic/game/sonic2/Sonic2SpecialStageProvider.java
@@ -1,16 +1,21 @@
 package uk.co.jamesj999.sonic.game.sonic2;
 
+import uk.co.jamesj999.sonic.game.ResultsScreen;
 import uk.co.jamesj999.sonic.game.SpecialStageAccessType;
+import uk.co.jamesj999.sonic.game.SpecialStageDebugProvider;
 import uk.co.jamesj999.sonic.game.SpecialStageProvider;
+import uk.co.jamesj999.sonic.game.sonic2.objects.SpecialStageResultsScreenObjectInstance;
 import uk.co.jamesj999.sonic.game.sonic2.specialstage.Sonic2SpecialStageManager;
 
 import java.io.IOException;
 
 /**
  * Sonic 2 special stage provider implementation.
- * Wraps the existing Sonic2SpecialStageManager with the SpecialStageProvider interface.
+ * Wraps the existing Sonic2SpecialStageManager with the SpecialStageProvider
+ * interface.
  *
- * <p>Sonic 2's special stages are accessed via star posts (checkpoints)
+ * <p>
+ * Sonic 2's special stages are accessed via star posts (checkpoints)
  * when the player has 50 or more rings. Each stage awards one of seven
  * Chaos Emeralds upon successful completion.
  */
@@ -62,7 +67,87 @@ public class Sonic2SpecialStageProvider implements SpecialStageProvider {
         manager.setEmeraldCollected(collected);
     }
 
-    // MiniGameProvider interface methods
+    // ==================== Debug Methods ====================
+
+    @Override
+    public boolean isSpriteDebugMode() {
+        return manager.isSpriteDebugMode();
+    }
+
+    @Override
+    public void toggleSpriteDebugMode() {
+        manager.toggleSpriteDebugMode();
+    }
+
+    @Override
+    public void cyclePlaneDebugMode() {
+        manager.cyclePlaneDebugMode();
+    }
+
+    @Override
+    public SpecialStageDebugProvider getDebugProvider() {
+        return manager.getDebugProvider();
+    }
+
+    // ==================== Alignment Test Methods ====================
+
+    @Override
+    public boolean isAlignmentTestMode() {
+        return manager.isAlignmentTestMode();
+    }
+
+    @Override
+    public void toggleAlignmentTestMode() {
+        manager.toggleAlignmentTestMode();
+    }
+
+    @Override
+    public void adjustAlignmentOffset(int delta) {
+        manager.adjustAlignmentOffset(delta);
+    }
+
+    @Override
+    public void adjustAlignmentSpeed(double delta) {
+        manager.adjustAlignmentSpeed(delta);
+    }
+
+    @Override
+    public void toggleAlignmentStepMode() {
+        manager.toggleAlignmentStepMode();
+    }
+
+    @Override
+    public void renderAlignmentOverlay(int viewportWidth, int viewportHeight) {
+        manager.renderAlignmentOverlay(viewportWidth, viewportHeight);
+    }
+
+    // ==================== Lag Compensation Methods ====================
+
+    @Override
+    public void renderLagCompensationOverlay(int viewportWidth, int viewportHeight) {
+        manager.renderLagCompensationOverlay(viewportWidth, viewportHeight);
+    }
+
+    @Override
+    public double getLagCompensation() {
+        return manager.getLagCompensation();
+    }
+
+    @Override
+    public void setLagCompensation(double factor) {
+        manager.setLagCompensation(factor);
+    }
+
+    // ==================== Results Screen ====================
+
+    @Override
+    public ResultsScreen createResultsScreen(int ringsCollected, boolean gotEmerald,
+            int stageIndex, int totalEmeraldCount) {
+        return new SpecialStageResultsScreenObjectInstance(
+                ringsCollected, gotEmerald, stageIndex, totalEmeraldCount);
+    }
+
+    // ==================== MiniGameProvider Methods ====================
 
     @Override
     public void initialize() throws IOException {
